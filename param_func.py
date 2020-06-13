7c1d10ef35551be23cfd03f9fa3e4324819b2aaf
sjo1403 <64970935+sjo1403@users.noreply.github.com>
2020-06-13 16:07:08 -0400

    Add files via upload
    This commit populates the entire spreadsheet with all field parameters with the exception of "volume." A bug that caused data to skip rows has been fixed.

--- param_func.py
+++ param_func.py
@@ -10,52 +10,106 @@ def param_func(paramData):
 		key += 1
 
 	#this dictionary creates a key-value pair for each parameter (key to parameter)
-	param = {"wellID" : 0, "rate" : 4, "DTW" : 5, "pH" : 7, "conductivity" : 8, "DO" : 11, "temp" : 12, "redox" : 13}
+	param = {"wellID" : 0, "time" : 1, "rate" : 4, "DTW" : 5, "pH" : 7, "conductivity" : 8, "DO" : 11, "temp" : 12, "redox" : 13}
 
 	index = 0
+	nextIndex = index + 1
 	pos = 0
 
 	currentWell = paramRow[index][param["wellID"]]
-	formerWell = currentWell
+	nextWell = paramRow[index + 1][param["wellID"]]
 
 	#the following statements populate lowFlowForm template with param info
 	for row in paramData:
 		if index == 0:
 			currentWell = paramRow[index][param["wellID"]]
-			formerWell = paramRow[0][param["wellID"]]
+			nextWell = paramRow[nextIndex][param["wellID"]]
 			
 			newbook = load_workbook(filename="GWS_form_" + currentWell + ".xlsx")
 			newSheet = newbook.active
+
+			#the following if/else statements prevent stray 0s from appearing
+			if newSheet['C' + str(pos + 26)].value != "0":
+				newSheet['A' + str(pos + 26)] = paramRow[index][param["time"]]
+				newSheet['C' + str(pos + 26)].number_format = 'HH:MM'
+
+			else:
+				newSheet['C' + str(pos + 26)].number_format = ''
+
 			newSheet['E' + str(pos + 26)] = paramRow[index][param["rate"]]
 			newSheet['I' + str(pos + 26)] = paramRow[index][param["temp"]]
+			newSheet['K' + str(pos + 26)] = paramRow[index][param["pH"]]
+			newSheet['M' + str(pos + 26)] = paramRow[index][param["conductivity"]]
+			newSheet['O' + str(pos + 26)] = paramRow[index][param["redox"]]
+			newSheet['Q' + str(pos + 26)] = paramRow[index][param["DO"]]
+			newSheet['U' + str(pos + 26)] = paramRow[index][param["DTW"]]
 			newbook.save(filename="GWS_form_" + currentWell + ".xlsx")
 
 			index += 1
+			nextIndex = index + 1
 			pos += 1
 
-		elif currentWell != formerWell:
+		elif currentWell != nextWell:
 			currentWell = paramRow[index][param["wellID"]]
+			if nextIndex > 69:
+				nextWell = currentWell
+
+			else:
+				nextWell = paramRow[nextIndex][param["wellID"]]
 			pos = 0
 			
 			newbook = load_workbook(filename="GWS_form_" + currentWell + ".xlsx")
 			newSheet = newbook.active
+			
+			#the following if/else statements prevent stray 0s from appearing
+			if newSheet['C' + str(pos + 26)].value != "0":
+				newSheet['A' + str(pos + 26)] = paramRow[index][param["time"]]
+				newSheet['C' + str(pos + 26)].number_format = 'HH:MM'
+
+			else:
+				newSheet['C' + str(pos + 26)].number_format = ''
+
 			newSheet['E' + str(pos + 26)] = paramRow[index][param["rate"]]
 			newSheet['I' + str(pos + 26)] = paramRow[index][param["temp"]]
+			newSheet['K' + str(pos + 26)] = paramRow[index][param["pH"]]
+			newSheet['M' + str(pos + 26)] = paramRow[index][param["conductivity"]]
+			newSheet['O' + str(pos + 26)] = paramRow[index][param["redox"]]
+			newSheet['Q' + str(pos + 26)] = paramRow[index][param["DO"]]
+			newSheet['U' + str(pos + 26)] = paramRow[index][param["DTW"]]
 			newbook.save(filename="GWS_form_" + currentWell + ".xlsx")
 
 			index += 1
-			formerWell = paramRow[index - 1][param["wellID"]]
+			nextIndex = index + 1
 			pos += 1
 		
 		else:
 			currentWell = paramRow[index][param["wellID"]]
-			formerWell = paramRow[index - 1][param["wellID"]]
+			if nextIndex > 69:
+				nextWell = currentWell
+
+			else:
+				nextWell = paramRow[nextIndex][param["wellID"]]
 			
 			newbook = load_workbook(filename="GWS_form_" + currentWell + ".xlsx")
 			newSheet = newbook.active
+			
+			#the following if/else statements prevent stray 0s from appearing
+			if newSheet['C' + str(pos + 26)].value != "0":
+				newSheet['A' + str(pos + 26)] = paramRow[index][param["time"]]
+				newSheet['C' + str(pos + 26)].number_format = 'HH:MM'
+
+			else:
+				newSheet['C' + str(pos + 26)].number_format = ''
+			
 			newSheet['E' + str(pos + 26)] = paramRow[index][param["rate"]]
 			newSheet['I' + str(pos + 26)] = paramRow[index][param["temp"]]
+			newSheet['K' + str(pos + 26)] = paramRow[index][param["pH"]]
+			newSheet['M' + str(pos + 26)] = paramRow[index][param["conductivity"]]
+			newSheet['O' + str(pos + 26)] = paramRow[index][param["redox"]]
+			newSheet['Q' + str(pos + 26)] = paramRow[index][param["DO"]]
+			newSheet['U' + str(pos + 26)] = paramRow[index][param["DTW"]]
 			newbook.save(filename="GWS_form_" + currentWell + ".xlsx")
 
 			index += 1
+			nextIndex = index + 1
 			pos += 1
